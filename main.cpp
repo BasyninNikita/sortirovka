2905f907cb3bec5b5f3f845353afb4a1bc83f488
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2017-10-27 12:19:41 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <utility>
+#include <sstream>
+using namespace std;
+void vvod(double *array,unsigned int size)
+{
+    string stroka;
+    getline(cin,stroka);
+    istringstream stream(stroka);
+    for(unsigned int i=0;i<size;i++)
+    {
+        stream>>array[i];
+    }
+}
+void selection_sort(double *array, unsigned int size){
+    for(unsigned int i=0; i<size-1; ++i){
+        double min=array[i+1];
+        unsigned int k=i+1;
+        for(unsigned int j=i+1; j<size; ++j){
+            if(array[j]<min){
+                min=array[j];
+                k=j;
+            }
+            if(min<array[i]){
+                swap(array[i], array[k]);
+            }
+        }
+    }
+}
+void vyvod(unsigned int size,double *array)
+{
+    for(unsigned int i=0;i<size;i++)
+    {
+        cout<<array[i]<<' ';
+    }
+}
+unsigned int size;
+
+int main()
+{
+    cin>>size;
+    cin.get();
+    double *array=new double[size];
+    vvod(array,size);
+    selection_sort(array,size);
+    vyvod(size,array);
+}
